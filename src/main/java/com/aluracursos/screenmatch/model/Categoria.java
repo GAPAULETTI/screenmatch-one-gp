bb129c5f62cf8cb6eaff1e1e62a07e5d659200d7
gpauletti <gastonpauletti@gmail.com>
2024-06-21 10:37:32 -0300

    ADD JPQuery

--- src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -1,21 +1,30 @@
 package com.aluracursos.screenmatch.model;
 
 public enum Categoria {
-    ACCION ("Action"),
-    ROMANCE ("Romance"),
-    DRAMA("Drama"),
-    COMEDIA("Comedy"),
-    CRIMEN("Crime");
+    ACCION ("Action", "Acción"),
+    ROMANCE ("Romance", "Romance"),
+    DRAMA("Drama", "Drama"),
+    COMEDIA("Comedy", "Comedia"),
+    CRIMEN("Crime", "Crimen");
 
     private String categoriaOmdb;
+    private String categoriaEsp;
 
-    Categoria (String categoriaOmdb){
+    Categoria (String categoriaOmdb, String categoriaEsp){
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEsp = categoriaEsp;
     }
 
     public static Categoria fromString(String text){
         for(Categoria categoria : Categoria.values()){
             if(categoria.categoriaOmdb.equalsIgnoreCase(text)){
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("No se encontró la categoría: " + text);
+    } public static Categoria fromEsp(String text){
+        for(Categoria categoria : Categoria.values()){
+            if(categoria.categoriaEsp.equalsIgnoreCase(text)){
                 return categoria;
             }
         }
--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -1,9 +1,6 @@
 package com.aluracursos.screenmatch.principal;
 
-import com.aluracursos.screenmatch.model.DatosSerie;
-import com.aluracursos.screenmatch.model.DatosTemporadas;
-import com.aluracursos.screenmatch.model.Episodio;
-import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.model.*;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
@@ -33,6 +30,9 @@ public class Principal {
                     1 - Buscar series 
                     2 - Buscar episodios
                     3 - Mostrar series buscadas
+                    4 - Buscar series por título
+                    5 - Top 5 mejores series
+                    6 - Buscar series por categoria
                                   
                     0 - Salir
                     """;
@@ -50,6 +50,14 @@ public class Principal {
                 case 3:
                     mostrarSeriesBuscadas();
                     break;
+                case 4:
+                    buscarSeriesPorTitulo();
+                    break;
+                case 5:
+                    buscarTop5Series();
+                    break;
+                case 6:
+                    buscarSeriesPorCategoria();
                 case 0:
                     System.out.println("Cerrando la aplicación...");
                     break;
@@ -116,6 +124,32 @@ public class Principal {
         series.stream()
                .sorted(Comparator.comparing(Serie::getGenero))
                .forEach(System.out::println);
+    }
+    private void buscarSeriesPorTitulo() {
+        System.out.println("Escribe el nombre de la serie que deseas buscar");
+        var nombreSerie = teclado.nextLine();
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainsIgnoreCase(nombreSerie);
+
+        if(serieBuscada.isPresent()){
+            System.out.println("La serie buscada es: " + serieBuscada.get());
+        }else{
+            System.out.println("Serie no encontrada");
+        }
+
+    }
+
+    private void buscarTop5Series(){
+        List<Serie> topSeries = repositorio.findTop5ByOrderByEvaluacionDesc();
+        topSeries.forEach(s -> System.out.println("Serie: " + s.getTitulo() + " Evaluación: " + s.getEvaluacion()));
+    }
+    private void buscarSeriesPorCategoria(){
+        System.out.println("Escribe el género que desea buscar: ");
+        var genero = teclado.nextLine();
+        var categoria = Categoria.fromEsp(genero);
+        List<Serie> seriePorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Las series de la categoria" + genero);
+        seriePorCategoria.forEach(System.out::println);
+
     }
 
 
